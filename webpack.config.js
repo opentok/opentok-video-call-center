61b2ff35bb93f119649e31994bba1a62dc0ae08f
Chris Tankersley <chris@ctankersley.com>
2023-02-10 16:08:30 -0500

    Updated any remaining dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 var path = require('path')
 var webpack = require('webpack')
+const { VueLoaderPlugin } = require("vue-loader");
+const TerserPlugin = require("terser-webpack-plugin");
+
 
 module.exports = {
   entry: ['./app.js'],
@@ -8,6 +11,7 @@ module.exports = {
     publicPath: '/dist/',
     filename: 'build.js'
   },
+  plugins: [new VueLoaderPlugin()],
   module: {
     rules: [
       {
@@ -53,22 +57,20 @@ module.exports = {
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
+  devtool: 'eval-source-map',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  module.exports.devtool = 'source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
       }
     }),
     new webpack.LoaderOptionsPlugin({
